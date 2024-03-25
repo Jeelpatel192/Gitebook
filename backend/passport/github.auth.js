56cfbc1caa40953e963b7eb5afd7af2be8f24fe4
Jeelpatel192 <jeelpatel192@gmail.com>
2024-03-25 18:44:56 +0530

    callback url updated

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -22,7 +22,7 @@ passport.use(
 		{
 			clientID: process.env.GITHUB_CLIENT_ID,
 			clientSecret: process.env.GITHUB_CLIENT_SECRET,
-			callbackURL: "/api/auth/github/callback",
+			callbackURL: "https://github-like-app-01ox.onrender.com/api/auth/github/callback",
 		},
 
 		async function (accessToken, refreshToken, profile, done) {
